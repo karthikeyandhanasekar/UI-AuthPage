80b47526fbdf8b35835872e02534f95e3665085c
karthikeyan <dkkarthik2000@gmail.com>
2022-08-05 16:31:00 +0530

    Validation Completing

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     $('input[type=text]').on('keyup', (e) => {
         validatetext(e, "text")
     })
-    $('input[type=email]').on('keyup', (e) => {
-        validemail(e, "text")
-    })
+    // $('input[type=email]').on('keyup', (e) => {
+    //     validemail(e, "text")
+    // })
     $('#password').on('keyup', (e) => {
         validpassword(e, "text")
     })
@@ -18,8 +18,100 @@ $(document).ready(function () {
 
 const onsubmit = (e) => {
     e.preventDefault()
-    alert('submitted')
-    document.querySelector("#registration").reset();
+
+    const namepattern = /^[A-Za-z]+$/
+    const emailpattern = pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    const forms = document.forms['registration']
+
+    let isvalid = true
+
+
+    // First Name Validation
+    const firstname = forms['firstname'].value
+
+    if (firstname.length === 0) {
+        document.querySelector('#firstname-error').innerHTML = 'First Name Requried'
+        isvalid = false
+        return
+    }
+    else if (!namepattern.test(firstname)) {
+        document.querySelector('#firstname-error').innerHTML = 'No Special Characters/Numbers'
+        isvalid = false
+    }
+    else
+        document.querySelector('#firstname-error').innerHTML = ''
+
+
+    // Last Name Validation
+    const lastname = forms['lastname'].value
+
+    if (lastname.length === 0) {
+        document.querySelector('#lastname-error').innerHTML = 'Last Name Requried'
+        isvalid = false
+        return
+    }
+    else if (!namepattern.test(lastname)) {
+        document.querySelector('#lastname-error').innerHTML = 'No Special Characters/Numbers'
+        isvalid = false
+    }
+    else
+        document.querySelector('#lastname-error').innerHTML = ''
+
+
+    // Email Validation
+    const email = forms['email'].value
+    if (email.length === 0) {
+        document.querySelector(`#email-error`).innerHTML = `Email Required`
+        isvalid = false
+        return
+    }
+    else if (!emailpattern.test(email) && email.length > 0) {
+        document.querySelector(`#email-error`).innerHTML = 'Enter Valid Email ID'
+        isvalid = false
+    }
+
+    else
+        document.querySelector(`#email-error`).innerHTML = ''
+
+
+    // Password Validation
+    const password = forms['password'].value
+    if (password.length === 0) {
+        document.querySelector(`#password-error`).innerHTML = `Password Required`
+        isvalid = false
+        return
+    }
+    else if (password.length < 4 || password.length > 10) {
+        document.querySelector(`#password-error`).innerHTML = 'Password Must have between 4 to 10 Characters'
+        isvalid = false
+    }
+
+    else
+        document.querySelector(`#password-error`).innerHTML = ''
+
+    // Confirm Password Validation
+    const repassword = forms['repassword'].value
+    if (password.length === 0) {
+        document.querySelector(`#password-error`).innerHTML = `Password Required`
+        isvalid = false
+        return
+    }
+    else if (password !== repassword && value.length > 0) {
+        document.querySelector(`#repassword-error`).innerHTML = " Password doesn't match."
+        isvalid = false
+    }
+
+    else
+        document.querySelector(`#repassword-error`).innerHTML = ''
+
+    if (isvalid) {
+        alert('Submitted')
+        document.querySelector("#registration").reset();
+        return
+    }
+
+    return
 
 }
 
@@ -28,38 +120,51 @@ const onsubmit = (e) => {
 
 
 const validatetext = (e) => {
+
+
     const value = e.target.value
     const id = e.target.id
-    const pattern = /\d/g // check if number contains
+    const name = e.target.name
+    let namevalue = 'Name', errormsg = 'No Special Character/Number'
+
+    let pattern = /^[A-Za-z]+$/g // check if special character/number contains
     document.querySelector(`#${id}-error`).innerHTML = ""
 
-    if (pattern.test(value)) {
-        document.querySelector(`#${id}-error`).innerHTML = "Invalid Value"
+    if (name === "email") {
+        namevalue = 'Email'
+        errormsg = 'Enter Valid Email ID'
+        pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    }
+
+
+    if (!pattern.test(value) && value.length > 0) {
+        document.querySelector(`#${id}-error`).innerHTML = errormsg
         return
     }
     if (value.length === 0) {
-        document.querySelector(`#${id}-error`).innerHTML = "Value Required"
+        document.querySelector(`#${id}-error`).innerHTML = `${namevalue} Required`
         return
     }
 }
 
-const validemail = (e) => {
-    const value = e.target.value
-    const id = e.target.id
-    const pattern = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/g // check if it valid email
+// const validemail = (e) => {
+//     const value = e.target.value
+//     const id = e.target.id
+//     const pattern = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/g // check if it valid email
 
-    document.querySelector(`#${id}-error`).innerHTML = ""
+//     document.querySelector(`#${id}-error`).innerHTML = ""
 
-    if (!pattern.test(value)) {
-        document.querySelector(`#${id}-error`).innerHTML = "Invalid Email"
-        return
-    }
-    if (value.length === 0) {
-        document.querySelector(`#${id}-error`).innerHTML = "Value Required"
-        return
-    }
+//     if (!pattern.test(value)) {
+//         document.querySelector(`#${id}-error`).innerHTML = "Invalid Email"
+//         return
+//     }
+//     if (value.length === 0) {
+//         document.querySelector(`#${id}-error`).innerHTML = "Email Required"
+//         return
+//     }
 
-}
+// }
 
 const validpassword = (e) => {
     const value = e.target.value
@@ -68,7 +173,7 @@ const validpassword = (e) => {
     document.querySelector(`#${id}-error`).innerHTML = ""
 
     if (value.length === 0) {
-        document.querySelector(`#${id}-error`).innerHTML = "Value Required"
+        document.querySelector(`#${id}-error`).innerHTML = "Password Required"
         return
     }
     if (value.length < 4 || value.length > 10) {
